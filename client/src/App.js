c6cd6c6077c60f1859ce033ac47032bd96376e70
impkdn <piyush.jld@gmail.com>
2024-03-24 11:20:41 +0530

    Basic Boiler Plate

---
+++ client/src/App.js
@@ -0,0 +1,47 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div class="hc">
+      <header class="hdr">
+        <div class="logoImg">
+
+        
+                <img src="./img/images.png" alt="RMtech">
+            
+        </div>
+        <nav style="color:red" class="navBar">
+            <ul>
+                <li>
+                  <a class="pageLinks" href="./pages/aboutUs.html" target="_blank">About Us</a>
+                  
+                </li>
+                <li>
+                  <a class="pageLinks" href="./pages/business.html" target="_blank">Business</a>
+                    
+                </li>
+                <li>
+                  <a class="pageLinks" href="./pages/contactUs.html" target="_blank">Contact Us</a>
+                  
+                </li>
+                <li>
+                  <a class="pageLinks" href="./pages/logIn.html" target="_blank">Log In</a>
+
+                    
+                </li>
+            </li>
+        </nav>
+        <div id="text">
+
+        </div>
+      </header>
+
+      <nav></nav>
+    </div>
+    </div>
+  );
+}
+
+export default App;
