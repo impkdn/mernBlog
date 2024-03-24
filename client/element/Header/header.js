0bfa8dc910ca0797f80a6ee856eb95b9884a13c2
impkdn <piyush.jld@gmail.com>
2024-03-24 20:16:04 +0530

    first change in Header element saved in git

--- client/element/Header/header.js
+++ client/element/Header/header.js
@@ -1,3 +1,38 @@
 function Header(params) {
-    
+    <div class="hc">
+      <header class="hdr">
+        <div class="logoImg">
+
+        
+                <img src="./img/images.png" alt="RMtech">
+            
+        </div>
+        <nav style="color:red" class="navBar">
+            <ul>
+                <li>
+                  <a class="pageLinks" href="./pages/aboutUs.html" target="_blank">About Us</a>
+                  
+                </li>
+                <li>
+                  <a class="pageLinks" href="./pages/business.html" target="_blank">Business</a>
+                    
+                </li>
+                <li>
+                  <a class="pageLinks" href="./pages/contactUs.html" target="_blank">Contact Us</a>
+                  
+                </li>
+                <li>
+                  <a class="pageLinks" href="./pages/logIn.html" target="_blank">Log In</a>
+
+                    
+                </li>
+            </li>
+        </nav>
+        <div id="text">
+
+        </div>
+      </header>
+
+      <nav></nav>
+    </div>
 }
